82e3c9c2b2e99ca274442c0bc55298e2b1347094
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-10-12 10:51:06 +0000

    [ADD]QuestionsController修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
   def top
+    if session[:questions].nil?
+      session[:questions] = Question.order("RANDOM()").limit(5).pluck(:id)
+      session[:current_question_index] = 0
+      session[:correct_answers_count] = 0
+    end
+
+
   end
 end
+
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,19 @@
 class QuestionsController < ApplicationController
-  before_action :set_questions, only: [:index, :show, :update]  # ここで確実にshowアクションの前に呼び出されるようにする
-
-  def index
-    session[:current_question_index] = 0   # 最初の質問から開始
-    session[:correct_answers_count] = 0    # 正解数をリセット
-    redirect_to question_path(@questions.first)  # 最初の質問へリダイレクト
-  end
+  
+  before_action :set_questions, only: [:index, :show, :update]
 
   def show
-    if session[:current_question_index].nil?
-      redirect_to questions_path and return
-    end
-
-    @question = @questions[session[:current_question_index]]  # 現在の質問を表示
-    @choices = @question.choices  # 質問に紐付いた選択肢を取得
+    @question = Question.find(session[:questions])
+    @choices = @question.choices
   end
   
   def update
     @question = @questions[session[:current_question_index]]
-
     if params[:choice_id].blank?
       flash[:alert] = "選択肢が選ばれていません"
       redirect_to question_path(@question) and return
     end
-  
+    
     selected_choice = Choice.find(params[:choice_id])
 
     if selected_choice == @question.correct_choice
@@ -32,21 +22,23 @@ class QuestionsController < ApplicationController
     else
       flash[:alert] = "不正解！"
     end
-
+    
     session[:current_question_index] += 1
-    if session[:current_question_index] < @questions.size
-      redirect_to question_path(@questions[session[:current_question_index]])
+    if session[:current_question_index] < session[:questions].length
+      redirect_to question_path(session[:questions][session[:current_question_index]])
     else
-      redirect_to quiz_sessions_result_path  # 全ての質問が終了したら結果画面へ
+      redirect_to quiz_sessions_result_path
     end
   end
 
   private
-
+  
   def set_questions
-    session[:questions] ||= Question.order("RANDOM()").limit(5).pluck(:id)  # セッションに保持された質問リストを取得、または新たにセット
-    @questions = Question.find(session[:questions])
+    if session[:questions].nil?
+      session[:questions] = Question.order("RANDOM()").limit(5).pluck(:id)
+      session[:current_question_index] = 0
+      session[:correct_answers_count] = 0
+    end
+    @questions = Question.includes(:choices).find(session[:questions])
   end
 end
-
-
